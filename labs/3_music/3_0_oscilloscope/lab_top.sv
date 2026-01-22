053f695246c960a23bc0c2a95435587a35f41c33
Evgeniy <zhenf@lenta.ru>
2026-01-22 23:02:18 +0300

    commit

--- labs/3_music/3_0_oscilloscope/lab_top.sv
+++ labs/3_music/3_0_oscilloscope/lab_top.sv
@@ -98,9 +98,10 @@ module lab_top
 
     // Excercise 1: Implement Zoom by x-axis with keys
     // Excercise 2: Optimize to reduce bits of bufy
-    assign white = x <= vldx                            // Design practice: Check if element of bufy is initialized
-            && (x>>2) <  (distance[19-:w_x])
-            && (y>>3) == (midy - bufy[(x>>2)])>>3       // draw bolder lines with shift by 3 bits
+    assign white = (x <= vldx)                            // Рисует только актуальные значения из буфера.
+                                                          // Design practice: Check if element of bufy is initialized
+            && (x>>2) < (distance[19-:w_x])             // растягивает буфер на весь экран и сглаживает
+            && (y>>3) == (midy - bufy[(x>>2)])>>3       // draw bolder lines with shift by 3 bits    
             && x < screen_width && y < screen_height;   // do not draw outside the screen
 
     always_ff @ (posedge clk)                           // Design practice: Separate always_ff blocks with reset and without
